19298dc5c466455e03b003756004120fd7720f0a
ltmontero <ltmleo@hotmail.com>
2019-02-25 20:03:33 -0300

    init

---
+++ src/olguinha/FXMLDocumentController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olguinha;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+/**
+ *
+ * @author lmleo
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    
+    
+    @FXML
+    private Button btnCadastrar;
+    private Label test;
+    
+    @FXML
+    private void abreJanela(String arquivoFXML){
+        try {
+           FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(arquivoFXML));
+           Parent root1 = (Parent) fxmlLoader.load();
+           Stage stage = new Stage();
+           stage.setScene(new Scene(root1));  
+           stage.show();
+           } catch(Exception e) {
+               e.printStackTrace();
+           }
+    }
+    
+    @FXML
+    private void clicouBotao(ActionEvent event) {
+        abreJanela("FXMLCadastro.fxml");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
